4653e32eefb99c162d045a081b408023c5dcce26
Alexey <qa.alexey@yandex.ru>
2024-02-24 17:47:14 +0300

    test cookie value 4

--- Ex11_test_cookie.py
+++ Ex11_test_cookie.py
@@ -3,10 +3,10 @@ class TestCookijes:
     def test_cookies(self):
         response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
 
-        cookie = dict(response.cookies)
-        print(cookie)
+        print(dict(response.cookies))
 
-        cookie_value = cookie["HomeWork"]
+        assert "HomeWork" in response.cookies, "There is no this cookie"
 
-        assert "HomeWork" in cookie, "There is no this cookie"
-        assert "hw_value" == cookie_value, "Cookie value is incorrect"
+        cookie_value = response.cookies.get("HomeWork")
+
+        assert cookie_value == "hw_value", "Cookie value is incorrect"
