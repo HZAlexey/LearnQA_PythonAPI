f8360a43b941ac36c60ad5d7556970c70dfcc25a
Alexey <qa.alexey@yandex.ru>
2024-03-03 20:54:47 +0300

    promlem

--- lib/assertions.py
+++ lib/assertions.py
@@ -3,7 +3,7 @@ import json
 
 class Assertions:
     @staticmethod
-    def assert_json_value_by_name(response: Response, name, expected_value, error_message):
+    def assert_json_value_by_name(response: Response, name, expected_value, error_message): # Проверяем, что ответ в JSON-формате
         try:
             response_as_dict = response.json()
         except json.JSONDecodeError:
@@ -11,3 +11,36 @@ class Assertions:
 
         assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
         assert response_as_dict[name] == expected_value, error_message
+
+    @staticmethod
+    def assert_json_has_key(response: Response, name): # Проверяем, что какое-то значение в JSON есть
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not on JSON format. Response text is '{response.text}'"
+
+        assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+
+    @staticmethod
+    def assert_json_has_keys(response: Response, names: list): # Проверяем, что конкретные значения (ключи) присутствуют в JSON
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not on JSON format. Response text is '{response.text}'"
+
+        for name in names:
+            assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
+
+    @staticmethod
+    def assert_json_has_not_key(response: Response, name): # Проверяем, что какого-то значения в JSON нет
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not on JSON format. Response text is '{response.text}'"
+
+        assert name not in response_as_dict, f"Response JSON shouldn't have key '{name}'. But it's present"
+
+    @staticmethod
+    def assert_code_status(response: Response, expected_status_code): # Проверяем, что статус код соответстует фактическому
+        assert response.status_code == expected_status_code, \
+            f"Unexpected status code! Expected {expected_status_code}. Actual: {response.status_code}"
--- lib/base_case.py
+++ lib/base_case.py
@@ -1,13 +1,14 @@
 import json.decoder
-
+from datetime import datetime
 from requests import Response
+
 class BaseCase:
     def get_cookie (self, response: Response, cookie_name):
         assert cookie_name in response.cookies, f"Cannot find cookie with name {cookie_name} in the last response"
         return response.cookies[cookie_name]
 
     def get_header (self, response: Response, headers_name):
-        assert headers_name in response.headers, f"Cannot finde header with the name {headers_name} in the last response"
+        assert headers_name in response.headers, f"Cannot find header with the name {headers_name} in the last response"
         return response.headers[headers_name]
 
     def get_json_value(self, response: Response, name):
@@ -19,3 +20,17 @@ class BaseCase:
         assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
 
         return response_as_dict[name]
+
+    def prepare_registration_data(self, email=None):
+        if email is None:
+            base_part = "learnqa"
+            domain = "example.com"
+            random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+            email = f"{base_part}{random_part}@{domain}"
+        return {
+            'username': 'learnqa',
+            'password': '123',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+        }
